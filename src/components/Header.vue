08b5acb6e43bf36a0ca8e587830979f713143076
Nekocup <d07405009@ems.npu.edu.tw>
2021-09-10 01:31:11 +0800

    layout: 新增大型banner

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
+        <div class="banner">
+            <img src="@/assets/image/header/banner.png" alt="">
+            <div class="title" id="home">
+                    <h1>最專業最快速的電腦維修</h1>
+                    <h6>給予顧客最好的服務品質</h6>
+                    <h6>良心的價格</h6>
+            </div>
+        </div>
     </div>
 </template>
 <script>
