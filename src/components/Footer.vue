42b38aae482876146e1937981f1cf65331aa9ed8
Nekocup <d07405009@ems.npu.edu.tw>
2021-09-11 19:37:13 +0800

    layout: 底部切版完成

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="footer">
+    <article>
+        <section>
+        <h6 id="contact">聯絡我們</h6>
+        <form action="">
+            <div class="inputContent">
+            <input type="text" name="姓名" id="" placeholder="姓名">
+            <input type="email" name="" id="" placeholder="信箱">
+            <textarea id="" cols="30" rows="10" placeholder="告訴我們你的問題"/>
+            </div>
+            <input type="submit" value="送出訊息">
+        </form>
+        </section>
+        <section>
+        <img src="@/assets/image/footer/LOGO2.png" alt="">
+        <hr>
+            <div class="footerFont">聯絡資訊</div>
+            <div class="footerFont">電話 : 0934031102</div>
+            <div class="footerFont">信箱 : bohau888@gmail.com</div>
+            <img src="@/assets/image/footer/QRCODE.png" alt="">
+            <div class="footerFont ">© 2021 FlashPig.All rights reserved.</div>
+        </section>
+    </article>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
