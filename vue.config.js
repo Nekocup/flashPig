9bbbfb0c742d8a642f941500b2bd6384b539d0f5
yuankai.ding <yuankai.ding@insyde.com>
2023-01-09 11:17:56 +0800

    ADD SPA

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/flashpig/'
-    : './'
+    : './',
+
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/'
+      ],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true
+    }
+  }
 }
